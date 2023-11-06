63371f88d229a581c24476f389874a523018e14d
Jacob Cohen <jcohen14@mail.pima.edu>
2023-11-06 16:44:11 -0700

    updated login page
    implementing client and server js

--- server/public_html/loginScript.js
+++ server/public_html/loginScript.js
@@ -1,9 +1,20 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
+const login = document.getElementById('signIn');
 
-signUpButton.addEventListener('click', () =>
-    container.classList.add('right-panel-active'));
+signUpButton.addEventListener('click', () => {
+    container.classList.add('right-panel-active');
+});
 
-signInButton.addEventListener('click', () =>
-    container.classList.remove('right-panel-active'));
+signInButton.addEventListener('click', () => {
+    container.classList.remove('right-panel-active');
+});
+
+document.getElementById("loadIndexButton").addEventListener("click", function() {
+    console.log("loadIndexButton clicked");
+    // Change the window's location to "index.html"
+    window.location.href = "successLogin_html/index.html";
+});
+
+
