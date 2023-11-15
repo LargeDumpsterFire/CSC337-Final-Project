c9118ebf573fc0e2b7ebcca036601c9065cb31cf
Josh Puhala <jpuhala@arizona.edu>
2023-11-14 22:59:00 -0700

    Updated server.js and index.html
    index.html : simple formatting adjustments. Just entering line breaks to prevent over-running code.
    
    server.js: Updated our imported modules for session handling, connecting to the db, hashing the passwords, and added code to actually connect to the db, implement session creation, and applied the bcrypt functionality to the signup and login functions.

--- server/server.js
+++ server/server.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+const path = require('node:path');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const store = new MongoDBStore({
+    uri: 'mongodb://localhost:27017/yourDBName', // Update connection string
+    collection: 'sessions'
+});
+const bcrypt = require('bcrypt');
 const app = express();
 const port = 3000;
 
+mongoose.connect('mongodb://localhost:27017/yourDBName', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}); // ^This needs updated with the connection string for the db
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(session({
+    secret: 'yourSecretKey',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+}));
 
 // Serve static files from the 'public_html' directory
 app.use(express.static(path.join(__dirname, 'public_html')));
@@ -19,9 +38,8 @@ app.post('/login', (req, res) => {
     const { username, password } = req.body;
 
     // Check if the user exists
-    const user = users.find(u => u.username === username && u.password === password);
-
-    if (user) {
+    const user = users.find(u => u.username === username);
+    if (user && bcrypt.compareSync(password, user.password)) {
         // Redirect to home.html on successful login
         res.send({ success: true });
     } else {
@@ -29,7 +47,6 @@ app.post('/login', (req, res) => {
         res.status(401).json({ success: false, message: 'Invalid credentials' });
     }
 });
-
 
 app.post('/signup', (req, res) => {
     const { username, password } = req.body;
@@ -39,7 +56,8 @@ app.post('/signup', (req, res) => {
         res.status(400).send({ success: false, message: 'Username already taken' });
     } else {
         // Add the new user
-        users.push({ username, password });
+        const hashedPassword = bcrypt.hashSync(password, 10);
+        users.push({ username, hashedPassword });
         res.send({ success: true });
     }
 });
@@ -52,3 +70,14 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
+
+/* This is as far as i got tonight, Some other things i'd like 
+to implement here in this file are:
+- Sanitize and validate inputs
+- Check routes for authentication
+- Add a logout route
+- Implement HTTPS
+- (if possible/time allows) Implement a logging system 
+    for security monitoring.
+*/
