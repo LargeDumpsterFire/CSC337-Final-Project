db438572db483e9a1f8100baf457e5d67edea175
Josh Puhala <jpuhala@arizona.edu>
2023-11-14 21:43:15 -0700

    Update indexScript.js
    Added more DOM manipulation and more (unfinished) route handling and event listeners for the on-page buttons.

--- server/public_html/js/indexScript.js
+++ server/public_html/js/indexScript.js
@@ -2,6 +2,8 @@ const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 const login = document.getElementById('signIn');
+const signUpForm = document.getElementById('signUpForm');
+const signInForm = document.getElementById('loginForm');
 
 signUpButton.addEventListener('click', () => {
     container.classList.add('right-panel-active');
@@ -10,70 +12,82 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
     container.classList.remove('right-panel-active');
 });
-function togglePasswordVisibility(checkbox, passwordId) {
-    const passwordInput = document.getElementById(passwordId);
 
-    if (checkbox.checked) {
-        passwordInput.type = 'text';
-    } else {
-        passwordInput.type = 'password';
-    }
-}
-
-document.getElementById('loginForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    // Perform login request
-    const email = document.getElementById('email-login').value;
-    const password = document.getElementById('password-login').value;
-
-    fetch('/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username: email, password }),
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Invalid credentials');
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log('Login successful');
-        window.location.href = '/home.html'; // Use href to allow going back
-    })
-    .catch(error => {
-        console.error('Login failed:', error.message);
-        alert('Login failed: ' + error.message);
-    });
-});
-
-document.getElementById('signUpForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    // Perform signup request
+// Function to handle sign-up form submission
+signUpForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // Prevent the default form submission
+    // Gather user input
     const username = document.getElementById('name-signup').value;
     const email = document.getElementById('login').value;
     const password = document.getElementById('signupPassword').value;
 
-    fetch('/signup', {
+    // Construct the data object
+    const signUpData = {
+        username: username,
+        email: email,
+        password: password
+    };
+
+    // Send sign-up data to the server using fetch API
+    fetch('/signupEndpoint', { /*endpoint here will need to be adjusted 
+                                to use our signup endpoint route.*/
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ username, email, password }),
+        body: JSON.stringify(signUpData)
     })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            // Redirect to the homepage on successful signup
-            console.log('Signup successful');
-            window.location.replace('/home.html');
-        } else {
-            alert('Signup failed: ' + data.message);
-        }
+    .then(response => {
+        // Handle the response from the server
+        // For example, show success/error messages to the user
+        // You might redirect the user upon successful signup
+    })
+    .catch(error => {
+        // Handle any errors that occurred during the fetch
     });
 });
 
+// Function to handle sign-in form submission
+signInForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Gather user input
+    const email = document.getElementById('email-login').value;
+    const password = document.getElementById('loginPassword').value;
+
+    // Construct the data object
+    const signInData = {
+        email: email,
+        password: password
+    };
+
+    // Send sign-in data to the server using fetch API
+    fetch('/loginEndpoint', { /*endpoint here will need to be adjusted 
+                                to use our login endpoint route.*/
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(signInData)
+    })
+    .then(response => {
+        // Redirect user to index.html(?) upon successful login
+        // Deal with session data here
+    })
+    .catch(error => {
+        // Handle any errors that occurred during the fetch
+        // Inform the user of any errors including invalid credentials
+    });
+});
+
+function togglePasswordVisibility(checkbox, passwordClass) {
+    var passwordInputs = document.getElementsByClassName(passwordClass);
+
+    Array.from(passwordInputs).forEach(function (passwordInput) {
+        if (checkbox.checked) {
+            passwordInput.type = 'text';
+        } else {
+            passwordInput.type = 'password';
+        }
+    });
+};
