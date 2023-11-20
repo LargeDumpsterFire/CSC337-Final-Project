52789a0d6381e7693fcbcdbde2f0fb752fad9cba
Josh Puhala <jpuhala@arizona.edu>
2023-11-19 22:15:51 -0700

    Updates to server.js, indexScript.js + homeScript.js
    Code refactoring and organizing for each file. Created userSchema to store users in the DB, tried to take a similar approach to handling the userProjects object, but this may need some work. indexScript now redirects to a refreshed index.html upon unsuccessful login. Added user/session authentication to specific routes.

--- server/public_html/js/indexScript.js
+++ server/public_html/js/indexScript.js
@@ -1,10 +1,90 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
-const login = document.getElementById('signIn');
 const signUpForm = document.getElementById('signUpForm');
 const signInForm = document.getElementById('loginForm');
 
+// Function to handle sign-up form submission
+const handleSignUp = async (event) => {
+    event.preventDefault();
+    const username = document.getElementById('name-signup').value;
+    const email = document.getElementById('login').value;
+    const password = document.getElementById('signupPassword').value;
+
+    const signUpData = {
+        username: username,
+        email: email,
+        password: password
+    };
+
+    try {
+        const response = await fetch('/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(signUpData)
+        });
+
+        if (response.ok) {
+            window.location.href = './index.html';
+        }
+        else if (response.status === 400) {
+            window.alert('Username already taken');
+            window.location.href = './index.html';
+        }
+        else {
+            window.alert('Error signing up');
+            window.location.href = './index.html';
+        }
+    } catch (error) {
+        // Handle fetch error
+        console.error('Error signing up:', error);
+        window.alert('Error signing up');
+        window.location.href = './index.html';
+    }
+};
+
+// Function to handle sign-in form submission
+const handleSignIn = async (event) => {
+    event.preventDefault();
+    const email = document.getElementById('email-login').value;
+    const password = document.getElementById('loginPassword').value;
+
+    const signInData = {
+        email: email,
+        password: password
+    };
+
+    try {
+        const response = await fetch('/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(signInData)
+        });
+
+        if (response.ok) {
+            window.alert('Logged in successfully');
+            window.location.href = './home.html';
+        } else if (response.status === 401) {
+            console.error('Invalid credentials');
+            window.alert('Invalid credentials');
+            window.location.href = './index.html';
+        } else {
+            console.error('Error logging in');
+            window.alert('Error logging in');
+            window.location.href = './index.html';
+        }
+    } catch (error) {
+        console.error('Error logging in:', error);
+        window.alert('Error logging in');
+        window.location.href = './index.html';
+    }
+};
+
+// Event listeners
 signUpButton.addEventListener('click', () => {
     container.classList.add('right-panel-active');
 });
@@ -13,72 +93,9 @@ signInButton.addEventListener('click', () => {
     container.classList.remove('right-panel-active');
 });
 
-// Function to handle sign-up form submission
-signUpForm.addEventListener('submit', (event) => {
-    event.preventDefault(); // Prevent the default form submission
-    // Gather user input
-    const username = document.getElementById('name-signup').value;
-    const email = document.getElementById('login').value;
-    const password = document.getElementById('signupPassword').value;
+signUpForm.addEventListener('submit', handleSignUp);
+signInForm.addEventListener('submit', handleSignIn);
 
-    // Construct the data object
-    const signUpData = {
-        username: username,
-        email: email,
-        password: password
-    };
-
-    // Send sign-up data to the server using fetch API
-    fetch('/signupEndpoint', { /*endpoint here will need to be adjusted 
-                                to use our signup endpoint route.*/
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(signUpData)
-    })
-    .then(response => {
-        // Handle the response from the server
-        // For example, show success/error messages to the user
-        // You might redirect the user upon successful signup
-    })
-    .catch(error => {
-        // Handle any errors that occurred during the fetch
-    });
-});
-
-// Function to handle sign-in form submission
-signInForm.addEventListener('submit', (event) => {
-    event.preventDefault(); // Prevent the default form submission
-
-    // Gather user input
-    const email = document.getElementById('email-login').value;
-    const password = document.getElementById('loginPassword').value;
-
-    // Construct the data object
-    const signInData = {
-        email: email,
-        password: password
-    };
-
-    // Send sign-in data to the server using fetch API
-    fetch('/loginEndpoint', { /*endpoint here will need to be adjusted 
-                                to use our login endpoint route.*/
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(signInData)
-    })
-    .then(response => {
-        // Redirect user to index.html(?) upon successful login
-        // Deal with session data here
-    })
-    .catch(error => {
-        // Handle any errors that occurred during the fetch
-        // Inform the user of any errors including invalid credentials
-    });
-});
 
 function togglePasswordVisibility(checkbox, passwordClass) {
     var passwordInputs = document.getElementsByClassName(passwordClass);
