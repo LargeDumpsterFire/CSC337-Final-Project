c9c1da5fbf2d2a28de95bf2b074e0d5e5cd673c6
Jacob Cohen <jcohen14@mail.pima.edu>
2023-11-19 21:13:41 -0700

    added a fab to home page

--- server/public_html/js/homeScript.js
+++ server/public_html/js/homeScript.js
@@ -1,53 +1,69 @@
-// Sample user projects
-const userProjects = [
-    //{ name: "Project 1", description: "Description for Project 1", date: "2023-11-13", imageUrl: "./img/TeamLogo.png" },
-    // Add more projects as needed
-    { name: "Project 2", description: "Description for Project 2", date: "2023-11-14", imageUrl: "./img/TeamLogo.png" },
-    { name: "Project 3", description: "Description for Project 3", date: "2023-11-15", imageUrl: "./img/TeamLogo.png" },
-    { name: "Project 4", description: "Description for Project 4", date: "2023-11-16", imageUrl: "./img/TeamLogo.png" },
-    { name: "Project 5", description: "Description for Project 5", date: "2023-11-17", imageUrl: "./img/TeamLogo.png" },
-    { name: "Project 6", description: "Description for Project 6", date: "2023-11-18", imageUrl: "./img/TeamLogo.png" },
-    { name: "Project 7", description: "Description for Project 7", date: "2023-11-19", imageUrl: "./img/TeamLogo.png" },
-    //{ name: "Project 8", description: "Description for Project 8", date: "2023-11-12", imageUrl: "./img/TeamLogo.png" },
-    { name: "Project 9", description: "Description for Project 9", date: "2023-11-18", imageUrl: "./img/TeamLogo.png" },
-];
+document.addEventListener("DOMContentLoaded", function () {
+  // Sample user projects
+  const userProjects = [
+      { name: "Project 1", description: "Description for Project 1", date: "2023-11-13", imageUrl: "./img/TeamLogo.png" },
+      { name: "Project 2", description: "Description for Project 2", date: "2023-11-14", imageUrl: "./img/TeamLogo.png" },
+      { name: "Project 3", description: "Description for Project 3", date: "2023-11-15", imageUrl: "./img/TeamLogo.png" },
+      { name: "Project 4", description: "Description for Project 4", date: "2023-11-16", imageUrl: "./img/TeamLogo.png" },
+      { name: "Project 5", description: "Description for Project 5", date: "2023-11-17", imageUrl: "./img/TeamLogo.png" },
+      { name: "Project 6", description: "Description for Project 6", date: "2023-11-18", imageUrl: "./img/TeamLogo.png" },
+      { name: "Project 7", description: "Description for Project 7", date: "2023-11-19", imageUrl: "./img/TeamLogo.png" },
+      { name: "Project 8", description: "Description for Project 8", date: "2023-11-12", imageUrl: "./img/TeamLogo.png" },
+      { name: "Project 9", description: "Description for Project 9", date: "2023-11-18", imageUrl: "./img/TeamLogo.png" },
+  ];
 
-// Function to create project cards
-function createProjectCard(project) {
-    const card = document.createElement("div");
-    card.className = "grid-item";
+  // Function to create project cards
+  function createProjectCard(project) {
+      const card = document.createElement("div");
+      card.className = "outside-image-box";
 
-    const projectImage = document.createElement("div");
-    projectImage.className = "project-image";
-    projectImage.innerHTML = `<img src="${project.imageUrl}" alt="Project Image">`;
-    
-    const textContainer = document.createElement("div");
-    textContainer.className = "text-container";
+      const projectImage = document.createElement("div");
+      projectImage.className = "project-image";
+      projectImage.innerHTML = `<img src="${project.imageUrl}" alt="Project Image">`;
 
-    const projectName = document.createElement("t");
-    projectName.id = "project-name";
-    projectName.innerText = project.name;
+      const textContainer = document.createElement("div");
+      textContainer.className = "image-box-text";
 
-    const lastUpdated = document.createElement("t");
-    lastUpdated.innerText = "Last updated";
+      const projectName = document.createElement("t");
+      projectName.id = "projectName";
+      projectName.innerText = project.name;
 
-    const date = document.createElement("t");
-    date.id = "date";
-    date.innerText = project.date;
+      const lastUpdated = document.createElement("t");
+      lastUpdated.innerText = "Last Edit:";
 
-    textContainer.appendChild(projectName);
-    textContainer.appendChild(lastUpdated);
-    textContainer.appendChild(date);
+      const date = document.createElement("t");
+      date.id = "date";
+      date.innerText = project.date;
 
-    card.appendChild(projectImage);
-    card.appendChild(textContainer);
+      textContainer.appendChild(projectName);
+      textContainer.appendChild(lastUpdated);
+      textContainer.appendChild(date);
 
-    return card;
-}
+      card.appendChild(projectImage);
+      card.appendChild(textContainer);
 
-// Populate the grid container with project cards
-const gridContainer = document.getElementById("grid");
-userProjects.forEach(project => {
-    const card = createProjectCard(project);
-    gridContainer.appendChild(card);
+      return card;
+  }
+
+  // Populate the grid container with project cards
+  const gridContainer = document.getElementById("wrapper");
+  userProjects.forEach(project => {
+      const card = createProjectCard(project);
+      gridContainer.appendChild(card);
+  });
+
+  const wrapper = document.getElementById("wrapper");
+
+  // Event listener for List and Grid view buttons
+  document.addEventListener("click", function (event) {
+      if (event.target.matches(".list")) {
+          // List view
+          event.preventDefault();
+          wrapper.classList.add("list");
+      } else if (event.target.matches(".grid")) {
+          // Grid view
+          event.preventDefault();
+          wrapper.classList.remove("list");
+      }
+  });
 });
